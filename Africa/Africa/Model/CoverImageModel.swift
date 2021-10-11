fc22352d8617c06ab548fd293fdb61b98ec44a21
krysxin <shanon_xin@outlook.com>
2021-10-12 01:17:43 +0900

    add cover-image json decoder

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by QINXIN SHU on 2021/10/12.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
--- Africa/Africa/View/CoverImageView.swift
+++ Africa/Africa/View/CoverImageView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    
+    // MARK: - BODY
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                    .scaledToFill() // Q: difference with the scaledToFit() ?
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        
+
     }
 }
+
+// MARK: - PREVIEW
 
 struct CoverImageView_Previews: PreviewProvider {
     static var previews: some View {
         CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
     }
 }
